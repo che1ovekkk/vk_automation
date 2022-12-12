57a682be3a72f25703f3103240f65b777c1a4b5e
che1ovekkk <che1ovekkk@mail..ru>
2022-12-12 13:54:43 +0400

    Вынес запуск браузера в отдельную функцию Мелкие исправления имён переменных

--- locators.py
+++ locators.py
@@ -7,5 +7,5 @@ search_button_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div[1]
 
 open_result_url = 'https://samara.hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80+%D0%BF%D0%BE+%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E&excluded_text=&salary=200000&currency_code=RUR&only_with_salary=true&experience=between3And6&employment=full&schedule=remote&order_by=relevance&search_period=0&items_on_page=50'
 apply_button_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[3]/div/div/a'
-already_registered_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[4]/div/div/div[1]/div[2]/div/div[2]/div/p'
-apply_whole_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[4]/div/div/div[1]/div[2]/div/div[2]'
+outside_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]'
+apply_whole_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[4]/div/div[1]/div[2]/div/div[2]'
--- test.py
+++ test.py
@@ -5,10 +5,15 @@ from locators import *
 from time import sleep
 
 
-def test_main():
+def open_browser(url):
     browser = webdriver.Chrome()
-    browser.get(url='https://samara.hh.ru')
+    browser.get(url=url)
     browser.implicitly_wait(3)
+    return browser
+
+
+def test_main():
+    browser = open_browser(url='https://samara.hh.ru')
 
     search_field = browser.find_element(By.NAME, 'text')
     search_field.send_keys('Инженер по тестированию')
@@ -17,9 +22,7 @@ def test_main():
 
 
 def test_filters():
-    browser = webdriver.Chrome()
-    browser.get(url='https://samara.hh.ru/vacancies/inzhener_po_testirovaniyu')
-    browser.implicitly_wait(3)
+    browser = open_browser(url='https://samara.hh.ru/vacancies/inzhener_po_testirovaniyu')
 
     filters_button = browser.find_element(By.CLASS_NAME, 'bloko-icon-link')
     filters_button.click()
@@ -37,13 +40,13 @@ def test_filters():
     browser.execute_script("arguments[0].scrollIntoView();", only_with_salary)
     only_with_salary.click()
 
-    expirience = browser.find_element(By.XPATH, expirience_field)
-    browser.execute_script("arguments[0].scrollIntoView();", expirience)
-    expirience.click()
+    experience = browser.find_element(By.XPATH, expirience_field)
+    browser.execute_script("arguments[0].scrollIntoView();", experience)
+    experience.click()
 
-    type = browser.find_element(By.XPATH, type_field)
+    type_of_work = browser.find_element(By.XPATH, type_field)
     browser.execute_script("arguments[0].scrollIntoView();", type)
-    type.click()
+    type_of_work.click()
 
     remote_work = browser.find_element(By.XPATH, remote_work_field)
     browser.execute_script("arguments[0].scrollIntoView();", remote_work)
@@ -58,9 +61,7 @@ def test_filters():
 
 
 def test_open_result():
-    browser = webdriver.Chrome()
-    browser.get(url=open_result_url)
-    browser.implicitly_wait(3)
+    browser = open_browser(url=open_result_url)
 
     first_vacancy = browser.find_element(By.CLASS_NAME, 'serp-item__title')
     first_vacancy.click()
@@ -77,11 +78,9 @@ def test_open_result():
     apply_email = browser.find_element(By.NAME, 'login')
     apply_email.send_keys('balykov_ms@mail.ru')
     # clicking outside so the cursor disappears
-    already_registered = browser.find_element(By.XPATH, already_registered_field)
-    already_registered.click()
+    outside = browser.find_element(By.XPATH, outside_field)
+    outside.click()
     sleep(1)
     apply_whole = browser.find_element(By.XPATH, apply_whole_field)
     browser.execute_script("arguments[0].scrollIntoView();", apply_whole)
     apply_whole.screenshot('filled_apply.png')
-
-#TODO вынести определение и запуск браузера в отдельную функцию с параметром урла
