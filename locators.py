6fab034cf203aa50b422cac918b5d94d8c71f2e8
che1ovekkk <che1ovekkk@mail..ru>
2022-12-27 14:18:58 +0400

    Revert "Добавил разметку для allure-отчётов; мелкие правки по тестам"
    This reverts commit 71509e11f99bd434c7f4b764be9fc76928b4b5ca.

--- locators.py
+++ locators.py
@@ -5,7 +5,7 @@ type_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div[1]/form/div
 remote_work_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div[1]/form/div[9]/div/div[2]/div[4]/label/span'
 search_button_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div[1]/form/div[15]/div[2]/button'
 
-open_result_url = 'https://samara.hh.ru/vacancy/74137276?from=vacancy_search_list&query=инженер%20по%20тестированию'
+open_result_url = 'https://samara.hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80+%D0%BF%D0%BE+%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E&excluded_text=&salary=200000&currency_code=RUR&only_with_salary=true&experience=between3And6&employment=full&schedule=remote&order_by=relevance&search_period=0&items_on_page=50'
 apply_button_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[3]/div/div/a'
 outside_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]'
 apply_whole_field = '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div[1]/div[4]/div/div[1]/div[2]/div/div[2]'
